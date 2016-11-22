ff8529e4342ca7e0cdd1f3834a97210927b3d94f
Thomas Brandenburger <mail@thomasbrandenburger.ch>
2016-11-22 20:41:37 +0100

    rest call to log access

--- socket-io/public/lib/jquery-fastlogin.js
+++ socket-io/public/lib/jquery-fastlogin.js
@@ -11,65 +11,106 @@
             siteId: "12345",
             siteTitle: "Demo"
         }, options);
-    
+        
         
         var socket = io('http://fastlogin.eu-2.evennode.com:80');
         
-        socket.on('fastlogin', function(data){
+        socket.on('fastlogin', function (data)
+        {
             var type = data.type;
-        
-            if(type == 'init') {
+            
+            if (type == 'init')
+            {
                 $('#output').append('channel id: ' + data.channelid);
                 
                 // create QR Code with the channel Id
                 createQRCode(data.channelid)
-            }else {
+            } else
+            {
+                //Fill the form input fields with the specific data
                 $(settings.usernameSelector).val(data.username);
                 $(settings.passwordSelector).val(data.password);
                 $('#success').show();
                 $('#submitbtn').hide();
+                
+                
+                logData = {
+                    siteId: settings.siteId,
+                    socketId: data.channelid,
+                    used: true
+                };
+                
+                // log the data through the rest api
+                logAccess(logData);
             }
-        
+            
         });
-    
-        function createQRCode(channelId) {
         
-            qrData = '{"siteid":' + settings.siteId + ',"channelid":"'+channelId+'","sitetitle":"' + settings.siteTitle + '"}';
+        function logAccess(logData)
+        {
+
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8080/api/accesses",
+                data: JSON.stringify(logData),
+                contentType: "application/json; charset=utf-8",
+                crossDomain: true,
+                dataType: "json",
+                success: function (data, status, jqXHR)
+                {
+                    
+                    console.log("successfully logged data");
+                },
+                
+                error: function (jqXHR, status)
+                {
+                    // error handler
+                    console.log(jqXHR);
+                    console.log('fail' + status.code);
+                }
+            });
+            
+            
+        }
         
+        function createQRCode(channelId)
+        {
+            qrData = '{"siteid":' + settings.siteId + ',"channelid":"' + channelId + '","sitetitle":"' + settings.siteTitle + '"}';
+            
             // QR CODE
             var options = {
                 // render method: 'canvas', 'image' or 'div'
                 render: 'canvas',
-            
+                
                 // version range somewhere in 1 .. 40
                 minVersion: 1,
                 maxVersion: 40,
-            
+                
                 // error correction level: 'L', 'M', 'Q' or 'H'
                 ecLevel: 'L',
-            
+                
                 // offset in pixel if drawn onto existing canvas
                 left: 0,
                 top: 0,
-            
+                
                 // size in pixel
                 size: 200,
-            
+                
                 // code color or image element
                 fill: '#000',
-            
+                
                 // background color or image element, null for transparent background
                 background: null,
-            
+                
                 // content
                 text: qrData,
-            
+                
                 // corner radius relative to module width: 0.0 .. 0.5
                 radius: 0,
-            
+                
                 // quiet zone in modules
                 quiet: 0,
-            
+                
                 // modes
                 // 0: normal
                 // 1: label strip
@@ -77,30 +118,30 @@
                 // 3: image strip
                 // 4: image box
                 mode: 0,
-            
+                
                 mSize: 0.1,
                 mPosX: 0.5,
                 mPosY: 0.5,
-            
+                
                 label: 'no label',
                 fontname: 'sans',
                 fontcolor: '#000',
-            
+                
                 image: null
             };
-        
+            
             $('#qr-container').empty().qrcode(options);
         }
         
         
         return this.each(function ()
         {
-/*            $(this).text(settings.text + ' (' + settings.version + ')');
-            
-    
-            if ( $.isFunction( settings.complete ) ) {
-                settings.complete.call( this );
-            }*/
+            /*            $(this).text(settings.text + ' (' + settings.version + ')');
+             
+             
+             if ( $.isFunction( settings.complete ) ) {
+             settings.complete.call( this );
+             }*/
             
         });
         
