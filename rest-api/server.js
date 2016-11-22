3e063de3b6be67c6390cfd1467f384edf5f41dde
Thomas Brandenburger <mail@thomasbrandenburger.ch>
2016-11-22 20:31:50 +0100

    multiple origins to allow

--- rest-api/server.js
+++ rest-api/server.js
@@ -21,7 +21,12 @@ app.use(bodyParser.json({
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    var allowedOrigins = ['http://localhost:3000', 'http://localhost:3500'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     // Request headers you wish to allow
