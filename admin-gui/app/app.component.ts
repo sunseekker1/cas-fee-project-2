47696d63f266100eb9b8f1b73ee73cdffa779268
Noel Bellon <nicam@gmx.net>
2016-11-27 14:13:03 +0100

    - material design lite

--- admin-gui/app/app.component.ts
+++ admin-gui/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MDL } from './material-design-lite-upgrade-element'; // Inofficial Angular Material Design Lite
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
+  // directives: [MDL]
 })
 export class AppComponent {
   title = 'FastLogin';
--- admin-gui/app/app.module.ts
+++ admin-gui/app/app.module.ts
@@ -4,12 +4,10 @@ import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms';
 import {HttpModule}    from '@angular/http';
+import { MaterialModule } from '@angular/material'; // Official Angular Material Module
+// import {Ng2MaterialNodeModule, Ng2MaterialModule} from "ng2-material"; // Inoffical Angular Material Module ng-material
 
 import {AppRoutingModule}     from './app-routing.module';
-
-// Imports for loading & configuring the in-memory web api
-import {InMemoryWebApiModule} from 'angular-in-memory-web-api/in-memory-web-api.module';
-import {InMemoryDataService}  from './in-memory-data.service';
 
 import {AppComponent}         from './app.component';
 import {DashboardComponent}   from './dashboard/dashboard.component';
@@ -31,7 +29,9 @@ import { ShortIdPipe } from './short-id.pipe'; // import our pipe here
     BrowserModule,
     FormsModule,
     HttpModule,
-    // InMemoryWebApiModule.forRoot(InMemoryDataService),
+    MaterialModule.forRoot(),
+    // Ng2MaterialNodeModule,
+    // Ng2MaterialModule,
     AppRoutingModule
   ],
   declarations: [
